1428cf41452b03c99f52c6e51290ab1e043b454b
isaccanedo <isaccanedo@gmail.com>
2022-01-29 10:25:06 -0300

    Initial commit

---
+++ kafka-consumer/src/main/java/com/isaccanedo/example/Employee.java
@@ -0,0 +1,14 @@
+package com.isaccanedo.example;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Employee {
+
+	private Integer id;
+	private String name;
+}
